7d2a32edca757ec0d9448f99b91a98cdbb1b20d1
AdrianCardin <acardinlozano@gmail.com>
2022-02-15 16:58:57 +0100

    quitando estilos

--- resources/views/dashboardCategorias.blade.php
+++ resources/views/dashboardCategorias.blade.php
@@ -1,22 +1,18 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Categorías') }}
-        </h2>
-    </x-slot>
+    
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
-                <div class="flex">
-                    <div class="flex-auto text-2xl mb-4">Listado de categorías</div>
+        <div class="">
+            <div class="">
+                <div class="">
+                    <div class=" text-2xl">Listado de categorías</div>
 
-                    <div class="flex-auto text-right mt-2">
+                    <div class=" mt-2">
                         <a href="/categoria" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Añadir
                             nueva categoría</a>
                     </div>
                 </div>
-                <table class="w-full text-md rounded mb-4">
+                <table class=" text-md rounded mb-4">
                     <thead>
                     <tr class="border-b">
                         <th class="text-left p-3 px-5">Categoría</th>
--- resources/views/dashboardPersonas.blade.php
+++ resources/views/dashboardPersonas.blade.php
@@ -1,22 +1,18 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Personas') }}
-        </h2>
-    </x-slot>
+
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
-                <div class="flex">
-                    <div class="flex-auto text-2xl mb-4">Listado de contactos</div>
+        <div class="">
+            <div class=" ">
+                <div class="">
+                    <div class=" text-2xl">Listado de contactos</div>
 
-                    <div class="flex-auto text-right mt-2">
+                    <div classmt-2">
                         <a href="/persona" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Añadir
                             nuevo contacto</a>
                     </div>
                 </div>
-                <table class="w-full text-md rounded mb-4">
+                <table class=" text-md rounded mb-4">
                     <thead>
                     <tr class="border-b">
                         <th class="text-left p-3 px-5">Estrella</th>
